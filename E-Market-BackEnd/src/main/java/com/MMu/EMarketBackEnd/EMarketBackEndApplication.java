e8e384a3624f9f454ced43f328f835f501455279
mohammadmu1 <M_Mustafa9@outlook.com>
2024-01-08 15:07:30 +0300

    upload to git hub

---
+++ E-Market-BackEnd/src/main/java/com/MMu/EMarketBackEnd/EMarketBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.MMu.EMarketBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMarketBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMarketBackEndApplication.class, args);
+	}
+
+}
---
+++ E-Market-BackEnd/src/test/java/com/MMu/EMarketBackEnd/EMarketBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MMu.EMarketBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMarketBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
